2f558cd1c6c374b6138585b414e4acdbe5e42ee2
Daniel Bruce <ircubic@gmail.com>
2009-07-06 15:21:42 +0200

    Initial code

---
+++ mogilestorage.py
@@ -0,0 +1,101 @@
+import os
+import mimetypes
+
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from StringIO import StringIO
+
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django.core.files.base import File
+from django.core.files.storage import Storage
+from django.utils.functional import curry
+from django.utils.text import force_unicode
+from django.core.files.storage import Storage
+
+try:
+    import mogilefs
+except ImportError:
+    raise ImproperlyConfigured, "Could not load mogilefs dependency.\
+    \nSee http://mogilefs.pbworks.com/Client-Libraries"
+
+class MogileFSStorage(Storage):
+    """MogileFS filesystem storage"""
+    def __init__(self, url):
+        self.url = url
+        components = url.path.split('/')
+        self.trackers = components[2].split(',')
+        self.domain = components[3]
+        self.clas = None
+        try:
+            self.clas = components[4]
+        except:
+            pass
+        self.client = mogilefs.Client(self.domain, self.trackers)
+    
+    def filesize(self, filename):
+        raise NotImplemented
+    
+    def path(self, filename):
+        raise NotImplemented
+    
+    def url(self, filename):
+        url = urlparse.urljoin(self.url, filename)
+    
+    def _open(self, filename, mode='rb'):
+        return MogileFileWrapper(filename, self, mode)
+
+    def _read(self, filename):
+        return self.client[filename]
+                                 
+    def exists(self, filename):
+        return filename in self.client
+    
+    def _save(self, filename, contents):
+        #filename = self.get_available_filename(filename)
+        
+        #contents = StringIO(contents)
+        success = self.client.send_file(filename, contents, self.clas)
+        if not success:
+            raise IOException("Unable to save file: %s" % filename)
+        return force_unicode(filename)
+    
+    def delete(self, filename): 
+        self.client.delete(filename)
+
+class MogileFileWrapper(File):
+    def __init__(self, name, storage, mode):
+        self._name = name
+        self._storage = storage
+        self._mode = mode
+        self._is_dirty = False
+        self._size = None
+        self.file = StringIO()
+        self.start_range = 0
+    
+    @property
+    def size(self):
+        if self._size is None:
+            self._read_in()
+        return self._size
+
+    def _read_in(self):
+        data = self._storage._read(self._name)
+        self.file = StringIO(data)
+        self._size = len(data)
+
+    def read(self, num_bytes=None):
+        self._read_in()
+        return self.file.getvalue()
+    
+    def write(self, content):
+        if 'w' not in self._mode:
+            raise AttributeError("File was not opened with write access.")
+        self.file = StringIO(content)
+        self._is_dirty = True
+        
+    def close(self):
+        if self._is_dirty:
+            self._storage._save(self._name, self.file)
+        self.file.close()
