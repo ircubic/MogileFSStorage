34b74004150c3dea40e8786abf73575aaa82b055
Daniel E. Bruce <ircubic@gmail.com>
2009-07-07 08:07:09 +0200

    Added __all__  statement

--- mogilestorage.py
+++ mogilestorage.py
@@ -10,9 +10,10 @@ from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 from django.core.files.base import File
 from django.core.files.storage import Storage
-from django.utils.functional import curry
 from django.utils.text import force_unicode
 from django.core.files.storage import Storage
+
+__all__ = ['MogileFSStorage', 'MogileFileWrapper']
 
 try:
     import mogilefs
